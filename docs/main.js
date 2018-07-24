84e9cf3b44b6ba23b0396ca6e0b3abd4355fa89d
turbographics2000 <turbographics2000@yahoo.co.jp>
2018-07-24 15:48:13 +0900

    Create main.js

---
+++ docs/main.js
@@ -0,0 +1,30 @@
+const myId = (new MediaStream).id;
+console.log(`myId:${myId}`);
+chrome.runtime.sendMessage('eiceogpklagmibnoccdincfglccflknk', { cap: true }, async streamId => {
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+            video: {
+                mandatory: {
+                    chromeMediaSource: 'desktop',
+                    chromeMediaSourceId: streamId
+                }
+            }
+        });
+    } catch (e) {
+        console.error(e);
+        return;
+    }
+    console.log(`streamId:${stream.id}`);
+    localView.srcObject = stream;
+    const peer = new Peer(myId, {
+        key: '01099bd8-1083-4c33-ba9b-564a1377e901'
+    });
+    peer.on('open', id => {
+        myIdDisp.textContent = id;
+        const room = peer.joinRoom('hoge_fuga_piyo', { mode: 'sfu', stream });
+        room.on('stream', stream => {
+            console.log(`room on stream peerId:${stream.peerId}`);
+            remoteView.srcObject = stream;
+        });
+    });
+});
